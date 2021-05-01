d3fe19ca16b14efba16ca8508a99f576d1257224
Tiawna Moses Account <tiawnamoses@yahoo.com>
2021-05-01 10:46:33 -0600

    finally fixed the date

--- js/index.js
+++ js/index.js
@@ -1,2 +1,3 @@
-document.querySelector('#theyear').textContent = Date.getFullYear;
+const date = new Date();
+document.querySelector('#theyear').textContent = date.getFullYear();
 document.querySelector('#lastmod').textContent = document.lastModified;
